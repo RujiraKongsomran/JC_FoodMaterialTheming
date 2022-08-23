5a340a433e76516f50035966f7463eb7e5c0341d
RujiraKongsomran <rujirabiwberry@gmail.com>
2022-08-23 20:04:07 +0700

    Create DessertInfo func

--- app/src/main/java/com/rujirakongsomran/jc_foodmaterialtheming/MainActivity.kt
+++ app/src/main/java/com/rujirakongsomran/jc_foodmaterialtheming/MainActivity.kt
@@ -4,12 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.rujirakongsomran.jc_foodmaterialtheming.ui.theme.JC_FoodMaterialThemingTheme
@@ -30,16 +34,32 @@ fun DessertIcon(@DrawableRes dessertIcon: Int, modifier: Modifier = Modifier) {
     Image(
         painter = painterResource(dessertIcon),
         contentDescription = null,
-        modifier = Modifier
+        modifier = modifier
             .size(64.dp)
             .padding(8.dp)
     )
+}
+
+@Composable
+fun DessertInfo(@StringRes dessertName: Int, dessertId: Int, modifier: Modifier = Modifier) {
+
+    Column {
+        Text(
+            text = stringResource(dessertName),
+            modifier = modifier
+                .padding(top = 8.dp)
+        )
+        Text(
+            text = stringResource(R.string.dessert_id, dessertId)
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     JC_FoodMaterialThemingTheme(darkTheme = false) {
-        DessertIcon(dessertIcon = R.drawable.dessert_icon1_apam_balik)
+        //DessertIcon(dessertIcon = R.drawable.dessert_icon1_apam_balik)
+        DessertInfo(dessertId = 53049, dessertName = R.string.dessert_name_1)
     }
 }
